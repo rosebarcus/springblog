475f8054037ddb4b2286effd985aa30d2d8f8f0c
Rose Barcus <angela.rose.barcus@gmail.com>
2021-07-21 11:55:57 -0500

    working on views exercises

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,21 +1,22 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
-
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String index() {
-        return "View all posts";
+    //all posts
+    @GetMapping(path = "/posts")
+    public String index(Model model) {
+        model.addAttribute("posts")
+        return "posts/index";
     }
-
+    //todo create a new post object and pass it to the view
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     @ResponseBody
     public String postByID(@PathVariable long id) {
-        return "View an individual post";
+        return "../posts/show.html";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
