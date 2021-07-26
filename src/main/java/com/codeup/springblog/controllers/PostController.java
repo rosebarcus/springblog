b93039231bcb3b94b4e1128f9c647c5f4be73445
Rose Barcus <angela.rose.barcus@gmail.com>
2021-07-26 16:57:50 -0500

    still getting white label errors for posts/edit

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -58,17 +58,17 @@ public class PostController {
     @GetMapping("/posts/create")
     @ResponseBody
     public String createForm() {
-        return "View form to create a post.";
+        return "/posts/create/";
     }
 
     // When you submit the form on the /posts/create page,
     // the information will be posted to the same URL
 //    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @PostMapping("/posts/create")
+    @PostMapping("posts/create")
     public String createPost(@RequestParam String title, @RequestParam String body) {
         User user = usersDao.getById(1L);
         Post post = new Post(title, body, user);
         postDao.save(post);
-        return "Creates new post.";
+        return "redirect:/posts";
     }
 }
