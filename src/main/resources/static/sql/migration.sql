72cd7d44d5efdda29f854dcc12130d529fd4b4af
Rose Barcus <angela.rose.barcus@gmail.com>
2021-07-29 15:54:01 -0500

    working on authorization exercise

--- src/main/resources/static/sql/migration.sql
+++ src/main/resources/static/sql/migration.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS springblog_db;
 
-CREATE USER springblog_user@localhost IDENTIFIED BY 'password';
+CREATE USER springblog_user@localhost IDENTIFIED BY '$2a$10$gvGkJ4OFS8nRehVjIWJ4l.GmVse3riES2H2qMhh0ex7ML01d5aaKy';
 GRANT ALL ON springblog_db.* TO springblog_user@localhost;
 
 SELECT user, host FROM mysql.user;
